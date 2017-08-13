edaceb5cf386748384016763a33fc6ee696eee5e
LAPTOP-48KIJ867\Langri <piotr.plebaniak@gmail.com>
2017-08-13 14:05:18 +0800

    no message

--- src/unstarter_views/layouts/files_cdnjs.blade.php
+++ src/unstarter_views/layouts/files_cdnjs.blade.php
@@ -9,4 +9,4 @@
 --}}    
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.3.7/js/tether.min.js"></script>
-{{ Html::script('https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js') }}
+{{ Html::script('https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js') }}
--- src/unstarter_views/layouts/files_local.blade.php
+++ src/unstarter_views/layouts/files_local.blade.php
@@ -1,13 +1,5 @@
 <!-- LOCAL FILES tf:b -->
 
-    <!-- css -->
-{{-- <link rel="stylesheet" async href="{{URL::to('css/normalize.min.css')}}"> --}}
-{{-- {!! Html::script('js/modernizr.custom.js') !!} --}}
-{{-- <link rel="stylesheet" type="text/css" href="{{URL::to('css/font-awesome.min.css')}}"> --}}
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.3.7/js/tether.min.js"></script>
 
-{{-- Html::script('js/jquery-2.2.3.min.js') --}}
-
-{{-- {!! Html::script('js/jquery-2.2.4.min.js') !!} --}}
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.3.7/js/tether.min.js"></script>
-
-{!! Html::script('js/jquery-3.1.1.min.js') !!}
+{!! Html::script('js/jquery-3.2.1.min.js') !!}
